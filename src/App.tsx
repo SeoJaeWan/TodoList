06d7a830d87c727174950ef427f8c77984fa4d2b
Ing-yeo_Developer <sjw7324@naver.com>
2022-05-31 17:30:22 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from 'styled-components/native';
+
+import {TodoListContextProvider} from './Context/TodoListContext';
+
+import Todo from './Screens/Todo';
+
+const Container = styled.View`
+  flex: 1;
+  background-color: #eee;
+`;
+
+const App = () => {
+  return (
+    <TodoListContextProvider>
+      <Container>
+        <Todo />
+      </Container>
+    </TodoListContextProvider>
+  );
+};
+
+export default App;
